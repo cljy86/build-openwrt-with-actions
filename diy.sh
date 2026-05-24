17f664c1e2b2d2fecf34ea591ea70041d69b2d3d
cljy86 <119552751+cljy86@users.noreply.github.com>
2026-05-24 11:54:43 +0800

    purge: remove kenzok8 feeds, only keep LAN IP change

--- diy.sh
+++ diy.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
-# Redmi AX3000 (IPQ5000) 自定义编译脚本
+# 纯净版 - 最小化配置
 
-# 设置默认 LAN IP
+# 设置默认 LAN IP 为 192.168.31.2
 sed -i 's/192.168.1.1/192.168.31.2/g' package/base-files/files/bin/config_generate
 
-# 设置默认主题为 Argon
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# 添加第三方软件源
-echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
-
-# 克隆 AdGuardHome
-git clone https://github.com/stevenjoezhang/luci-app-adguardhome.git package/ADGH 2>/dev/null || true
-
-echo "=== diy.sh 执行完成 ==="
+echo "=== diy.sh 执行完成（纯净版，无第三方插件） ==="
