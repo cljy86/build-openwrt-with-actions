eadf69ba328ec02510a5b214b5b84b75ed2d4030
cljy86 <119552751+cljy86@users.noreply.github.com>
2026-05-24 11:36:49 +0800

    feat: AX3000 自定义编译脚本

--- diy.sh
+++ diy.sh
@@ -1,20 +1,17 @@
-# 请在下方输入自定义命令(一般用来安装第三方插件)(可以留空)
+#!/bin/bash
+# Redmi AX3000 (IPQ5000) 自定义编译脚本
 
-# 编辑默认的lan口ip地址
-sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+# 设置默认 LAN IP
+sed -i 's/192.168.1.1/192.168.31.2/g' package/base-files/files/bin/config_generate
 
-# 编辑默认的主题
+# 设置默认主题为 Argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# 编辑默认的luci显示的固件名称
-#sed -i 's/OpenWrt/ZWRT/g' package/base-files/files/bin/config_generate
-#sed -i 's/ImmortalWrt/ZWRT/g' package/base-files/files/bin/config_generate
+# 添加第三方软件源
+echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 
-# 添加额外的软件包，echo 方式和git clone 方式二选一即可
-#echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git UA3F https://github.com/SunBK201/UA3F.git' >>feeds.conf.default
-#git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
-#git clone https://github.com/kenzok8/small.git package/small
-#git clone https://github.com/SunBK201/UA3F.git package/UA3F
-git clone https://github.com/stevenjoezhang/luci-app-adguardhome.git package/ADGH
+# 克隆 AdGuardHome
+git clone https://github.com/stevenjoezhang/luci-app-adguardhome.git package/ADGH 2>/dev/null || true
+
+echo "=== diy.sh 执行完成 ==="
